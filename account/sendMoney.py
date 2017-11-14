96bc1b14d4c51870339e2773d8f08bbb382a4b19
Alex <alessio.battistutta@gmail.com>
2017-11-14 09:36:02 +0000

    Base classes

--- account/sendMoney.py
+++ account/sendMoney.py
@@ -1,2 +1,38 @@
+from base.BasePost import BasePost as Parent
 
-# to be defined
+class SendMoney(Parent):
+    def __init__(self,recipient = None, amountNQT=0, publicKey = None, secretPhrase=None, feeNQT = None, deadline = 0, message = None ):
+
+        self.recipient = recipient
+        self.amountNQT = amountNQT
+        self.publicKey = publicKey
+        self.secretPhrase = secretPhrase
+        if feeNQT == 0:
+            self.feeNQT = 100000000
+        else:
+            self.feeNQT = feeNQT
+        self.deadline = deadline
+        self.message = message
+
+        # Initialize dictionary
+        self.data = {}
+
+        ## Create data dictionary
+        
+        self.data["recipient"] = self.recipient
+        self.data["amountNQT"] = self.amountNQT
+        if publicKey:
+            self.data["publicKey"] = self.publicKey
+        if secretPhrase:
+            self.data["secretPhrase"] = self.secretPhrase
+        self.data["feeNQT"] = self.feeNQT
+        self.data["deadline"] = self.deadline
+        self.data["message"] = self.message
+        super(SendMoney, self).__init__(rt = "sendMoney", data=self.data)
+
+    def run(self):
+        super(SendMoney, self).run()               # calls 'BasePost.run()'
+
+    def getData(self, key=None):
+        return super(SendMoney, self).getData(key)    # calls 'BasePost.getData()'
+
