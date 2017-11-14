590e99b0636155b8db2696aa5faa7419e38d58c9
Alex <alessio.battistutta@gmail.com>
2017-11-14 09:28:20 +0000

    Base classes

--- base/BasePost.py
+++ base/BasePost.py
@@ -14,19 +14,28 @@ class BasePost(object):
         # self.data = {"requestType": self.requestType, "account": self.account}
         self.data = data
 
+        print(self.data)
     def param(self):
         pass
 
     def run(self):
         self.response = requests.post(self.url, data=self.data, headers=self.headers)
-        self.dataDict = \
-            json.loads(self.response.text)
+        self.dataDict = json.loads(self.response.text)
 
     def getData(self, key=None):
         if key:
             return self.dataDict[key]
         else:
             return self.dataDict
+
+    def getRequestType(self):
+        if self.requestType:
+            return self.requestType
+        else:
+            if self.data["requestType"]:
+                return self.data["requestType"]
+            else:
+                return None
 
     def auth(self, authObject):
         pass
