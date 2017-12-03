89a2c9aa8b065a9dafc72d4d2d9b8250d6e33fa7
Alex <alessio.battistutta@gmail.com>
2017-12-03 10:34:14 +0000

    tbd

--- base/Recip.py
+++ base/Recip.py
@@ -15,6 +15,8 @@ class Recip(object):
         :param sqrtassist: int
 
         """
+        print("Recip")
+
         t0 = Long10()
         t1 = Long10()
         t2 = Long10()
@@ -23,8 +25,9 @@ class Recip(object):
         i = 0
 
         """/ *the chain for x ^ (2 ^ 255 - 21) is straight from djb's implementation */ """
-
+        temp = t1
         sqr(t1, x)                          # 2 == 2 * 1
+        print(temp == t1)
         sqr(t2, t1)                         # 4 == 2 * 2
         sqr(t0, t2)                         # 8 == 2 * 4
         mul(t2, t0, x)                      # 9 == 8 + 1
@@ -51,52 +54,52 @@ class Recip(object):
         sqr(t4, t3)                         # 2 ^ 22 - 2 ^ 2
 
         for i in range(10):
-            sqr(t3, t4);
-            sqr(t4, t3);
+            sqr(t3, t4)
+            sqr(t4, t3)
                                             # t4 * / / * 2 ^ 40  - 2 ^ 20
 
-        mul(t3, t4, t1);                    # 2 ^ 40 - 2 ^ 0
+        mul(t3, t4, t1)                     # 2 ^ 40 - 2 ^ 0
 
         for i in range(5):
-            sqr(t1, t3);
-            sqr(t3, t1);
+            sqr(t1, t3)
+            sqr(t3, t1)
                                             # t3 * / / * 2 ^ 50  - 2 ^ 10
 
-        mul(t1, t3, t2);                    # 2 ^ 50 - 2 ^ 0
-        sqr(t2, t1);                        # 2 ^ 51 - 2 ^ 1
-        sqr(t3, t2);                        # 2 ^ 52 - 2 ^ 2
+        mul(t1, t3, t2)                     # 2 ^ 50 - 2 ^ 0
+        sqr(t2, t1)                         # 2 ^ 51 - 2 ^ 1
+        sqr(t3, t2)                         # 2 ^ 52 - 2 ^ 2
 
         for i in range(25):
-            sqr(t2, t3);
-            sqr(t3, t2);
+            sqr(t2, t3)
+            sqr(t3, t2)
                                             # t3 * / / * 2 ^ 100 - 2 ^ 50
 
-        mul(t2, t3, t1);                    # 2 ^ 100 - 2 ^ 0
-        sqr(t3, t2);                        # 2 ^ 101 - 2 ^ 1
-        sqr(t4, t3);                        # 2 ^ 102 - 2 ^ 2
+        mul(t2, t3, t1)                     # 2 ^ 100 - 2 ^ 0
+        sqr(t3, t2)                         # 2 ^ 101 - 2 ^ 1
+        sqr(t4, t3)                         # 2 ^ 102 - 2 ^ 2
 
         for i in range(50):
-            sqr(t3, t4);
-            sqr(t4, t3);
+            sqr(t3, t4)
+            sqr(t4, t3)
                                             # t4 * / / * 2 ^ 200 - 2 ^ 100
 
-        mul(t3, t4, t2);                    # 2 ^ 200 - 2 ^ 0
+        mul(t3, t4, t2)                     # 2 ^ 200 - 2 ^ 0
 
         for i in range(25):
-            sqr(t4, t3);
-            sqr(t3, t4);
+            sqr(t4, t3)
+            sqr(t3, t4)
                                             # t3 * / / * 2 ^ 250 - 2 ^ 50
 
-        mul(t2, t3, t1);                    # 2 ^ 250 - 2 ^ 0
-        sqr(t1, t2);                        # 2 ^ 251 - 2 ^ 1
-        sqr(t2, t1);                        # 2 ^ 252 - 2 ^ 2
+        mul(t2, t3, t1)                     # 2 ^ 250 - 2 ^ 0
+        sqr(t1, t2)                         # 2 ^ 251 - 2 ^ 1
+        sqr(t2, t1)                         # 2 ^ 252 - 2 ^ 2
 
         if (sqrtassist != 0):
-            mul(y, x, t2);                  # 2 ^ 252 - 3
+            mul(y, x, t2)                   # 2 ^ 252 - 3
         else:
-            sqr(t1, t2);                    # 2 ^ 253 - 2 ^ 3
-            sqr(t2, t1);                    # 2 ^ 254 - 2 ^ 4
-            sqr(t1, t2);                    # 2 ^ 255 - 2 ^ 5
-            mul(y, t1, t0);                 # 2 ^ 255 - 21
+            sqr(t1, t2)                     # 2 ^ 253 - 2 ^ 3
+            sqr(t2, t1)                     # 2 ^ 254 - 2 ^ 4
+            sqr(t1, t2)                     # 2 ^ 255 - 2 ^ 5
+            mul(y, t1, t0)                  # 2 ^ 255 - 21
 
 
