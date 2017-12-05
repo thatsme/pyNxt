b44353887222c7c4b039f3f1b6ead30176ef116b
Alex <alessio.battistutta@gmail.com>
2017-12-05 22:07:17 +0000

    tbd

--- curve25519/Long10.py
+++ curve25519/Long10.py
@@ -13,8 +13,8 @@ class Long10(object):
         self.v8 = i8
         self.v9 = i9
 
-    def printAll(self):
-        print(self.v0,self.v1,self.v2,self.v3,self.v4,self.v5,self.v6,self.v7,self.v8,self.v9)
+    def __str__(self):
+        return "({0},{1},{2},{3},{4},{5},{6},{7},{8},{9})".format(self.v0,self.v1,self.v2,self.v3,self.v4,self.v5,self.v6,self.v7,self.v8,self.v9)
 
     @property
     def _0(self):
