50ea348fb7d6dcdabf46fc3ef5e77f02e9eb71b0
Alex <alessio.battistutta@gmail.com>
2017-12-07 14:47:37 +0000

    tbd

---
+++ phasing/approveTransaction.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from base.BasePost import BasePost as Parent
+
+class ApproveTransaction(Parent):
+    def __init__(self, transactionFullHash=None, revealedSecret = None, revealedSecretIsText=None, publicKey = None, secretPhrase=None, feeNQT = None, deadline = 0, referencedTransactionFullHash = None, broadcast=True, phasing = None, message=None ):
+        """
+            ApproveTransaction approve (vote for) a phased transaction. POST only.
+            POST only.
+
+            ApproveTransaction take a default 1 parameter as explained in NXT API Documentation
+
+            https://nxtwiki.org/wiki/The_Nxt_API#Approve_Transaction
+
+            REQUEST
+            :param transactionFullHash : is the full hash of the transaction to be approved (may be used up to 10 times per API request)
+            :param revealedSecret is the secret phrase (required only for phasingVotingModel 5, refer to Create Phasing Poll)
+            :param revealedSecretIsText is a way of specifying whether revealedSecret is text or binary.
+            :param publicKey *: publicKey of sender accounts ( does not get in broadcast )
+            :param secretPhrase **: secret Phrase of sender accounts
+            :param feeNQT : fee for sending transaction (R) if 0 minimum is set ( 100000000 NQT )
+            :param deadLine : is the deadline (in minutes) for the transaction to be confirmed, 32767 minutes maximum ( if 0, 60 )
+            :param referencedTransactionFullHash : creates a chained transaction, meaning that the current transaction cannot be confirmed
+                                            unless the referenced transaction is also confirmed (O)
+            :param broadcast : is set to false to prevent broadcasting the transaction to the network (B) (O)
+            :param phasing : phasing object ( check base/Phasing.py ) (O) (WP)
+            :param message : message object ( check base/message.py ) (O) (WP)
+
+            RESPONSE
+            signatureHash : is a SHA-256 hash of the transaction signature (S)
+            unsignedTransactionBytes : are the unsigned transaction bytes (S)
+            transactionJSON : is a transaction object (O)  (refer to Get Transaction for details)
+            broadcasted : is true if the transaction was broadcast, false otherwise (B)
+            requestProcessingTime : is the API request processing time (in millisec)  (N)
+            transactionBytes :  are the signed transaction bytes (S)
+            fullHash : is the full hash of the signed transaction (S)
+            transaction : is the ID of the newly created transaction (S)
+            requestProcessingTime : is the API request processing time (N) (in millisec)
+
+            Legenda
+                ° the parameter are interchangeable on
+                * if you use the secretPhrase , the transaction is immediately broadcasted to network
+                ** if you use the publicKey, you create an unsigned Transaction, and you need to sign and broardcast
+                *** for buying
+                (R) Required
+                (O) Optional
+                (N) Number
+                (S) String
+                (B) Boolean
+                (A) Array
+                (O) Object
+                >   Array Element
+                (WP) Wrapper Meta-parameter
+
+
+        """
+
+        # Required parameters
+        self.transactionFullHash = transactionFullHash
+        self.revealedSecret = revealedSecret
+        self.revealedSecretIsText = revealedSecretIsText
+
+        self.publicKey = publicKey
+        self.secretPhrase = secretPhrase
+        self.referencedTransactionFullHash = referencedTransactionFullHash
+        self.broadcast = broadcast
+
+        if feeNQT == 0:
+            self.feeNQT = 100000000
+        else:
+            self.feeNQT = feeNQT
+
+        if deadline == 0:
+            self.deadline = 60
+        else:
+            self.deadline = deadline
+
+        self.phasing = phasing
+        self.message = message
+
+        # Initialize dictionary
+        self.data = {}
+
+        ## Create data dictionary
+        self.data["transactionFullHash"] = self.transactionFullHash
+        self.data["revealedSecret"] = self.revealedSecret
+        self.data["revealedSecretIsText"] = self.revealedSecretIsText
+
+
+        super(ApproveTransaction, self).__init__(rt="approveTransaction", data=self.data, phasing=self.phasing, message=self.message)
+
+    def run(self):
+        super(ApproveTransaction, self).run()                # calls 'BasePost.run()'
+
+    def getData(self, key=None):
+        """
+        :param key: dictionary key, if None return the whole dictionary
+        :return: dictionary of data
+        """
+        return super(ApproveTransaction, self).getData(key)  # calls 'BasePost.getData()'
+
