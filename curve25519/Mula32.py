48195891d672c1776e5556416c1134c7146dd987
Alex <alessio.battistutta@gmail.com>
2017-12-11 23:39:55 +0000

    Version Alpha 0.0.03

--- curve25519/Mula32.py
+++ curve25519/Mula32.py
@@ -1,9 +1,10 @@
 from curve25519.Mula_Small import Mula_Small as mula_small
 from curve25519.Packl_ctypes import Packl_ctypes as packl_ctypes
+from curve25519.ToHexString import ToHexString as ToHexString
 
 class Mula32(object):
 
-    def __init__(self, p, x, y, t, z):
+    def __init__(self, p, x, y, t, z, MDEBUG = False):
         """
 
         :param p: list[]
@@ -22,12 +23,18 @@ class Mula32(object):
 
         for i in range(t):
             zy = z * (y[i] & 0xFF)
-            w += mula_small(p, p, i, x, n, zy).value + (p[i+n] & 0xFF) + zy * (x[n] & 0xFF)
+            w += mula_small(p, p, i, x, n, zy, False).value + (p[i+n] & 0xFF) + zy * (x[n] & 0xFF)
             ## print("w, and packl_ctypes w ", w, packl_ctypes(w).value, type(w), type(packl_ctypes(w).value))
             p[i+n] = packl_ctypes(w).value
             w >>= 8
 
+        ## Porc .... fck java
+        i += 1
+        ##
         p[i + n] = packl_ctypes((w + (p[i + n] & 0xFF))).value
+        if MDEBUG:
+            print(i,t,"               |_|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|_|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-")
+            print(i,t," msma --> (p) ", ToHexString(p).getString())
 
         self.value = w >> 8
 
