ba871f72c7dd11fc8671f452854274ee75151ca0
Alex <alessio.battistutta@gmail.com>
2017-12-12 18:50:17 +0000

    Version Alpha 0.0.03

--- transactions/signTransactionOffline.py
+++ transactions/signTransactionOffline.py
@@ -1,27 +1,104 @@
 import codecs
-import curve25519.Curve25519 as curve
+from curve25519.Sign import Sign as sign
+from curve25519.Keygen import Keygen as ki
+#import drop.Curve25519 as curve
 from hashlib import sha256
+from curve25519.ToHexString import ToHexString
+from curve25519.ParseHexString import ParseHexString as ParseHexString
+import struct
+from copy import deepcopy
 
 class SignTransactionOffline(object):
+
     def __init__(self, secretPhrase=None, transaction = None):
         self.secretPhrase = secretPhrase
-        #self.transaction = transaction
+        self.transaction = ParseHexString(transaction).getData()
+        self.P = [0] * 32
+        self.h = [0] * 32
+        self.m = [0] * 32
+        self.v_list = [0] * 32
+        self.sct = None
+        self.pk_1 = None
+        self.s_1 = [0] * 32
+        self.pk_2 = None
+        self.s_2 = [0] * 32
+        self.hash_m = sha256()
+        self.hash_h = sha256()
+        self.hash_x = sha256()
+        self.MDEBUG = False
 
-        self.transaction = sha256(transaction).digest()
-        self.secret = sha256(self.secretPhrase).digest()
+    def bytesToList(self, b):
+        return list(struct.unpack('=32b', b))
 
-        self.myCurve = curve.Curve25519()
-        self.verification_key = None
-        self.signing_key = None
-        self.secret_clamped = None
+    def listToBytes(self, l):
+        return bytes(b % 256 for b in l)
 
-        self.verification_key, self.signing_key, self.secret_clamped = self.myCurve.curve25519_eckcdsa_keygen(self.secret)
-        self.signature = None
-        self.verified = None
+    def twoscomplement_to_unsigned(self, i):
+        return i % 256
+
     def run(self):
+        secret = sha256(self.secretPhrase.encode('utf-8')).hexdigest()
+        self.sct = ParseHexString(secret).getData()
 
-        self.signature = self.myCurve.kcdsa_sign(self.transaction, self.secret)
-        self.verified = self.myCurve.kcdsa_verify(self.signature, self.transaction, self.verification_key)
+        #####
+        self.keygen = ki(self.P, self.s_1, self.sct)
+        self.pk_1 = self.keygen.publicKey
+        #self.s_1 = self.keygen.s
+        ####
+
+        if self.MDEBUG:
+            print("Secret Key first keygen ", ToHexString(self.pk_1).getString())
+            print("self.s first keygen ", ToHexString(self.s_1).getString())
+
+        self.hash_m.update(self.listToBytes(self.transaction))
+        self.m_bytes = self.hash_m.digest()
+
+        if self.MDEBUG:
+            print("self.m bytes ", self.m_bytes)
+            print("len of self.m bytes ", len(self.m_bytes))
+
+        # Digest of self.s in self.x
+
+        self.hash_x.update(self.m_bytes)
+        self.hash_x.update(self.listToBytes(self.s_1))
+        self.x_bytes = self.hash_x.digest()
+        self.x_list = self.bytesToList(self.x_bytes)
+
+        if self.MDEBUG:
+            print("self.x bytes ", self.x_bytes)
+            print("self.x list ", self.x_list)
+            print("len of self.x bytes ", len(self.x_bytes))
+
+        # New Keygen with  s at None
+        Y_list = [0] * 32
+        self.keygen2 = ki(Y_list, None, self.x_list)
+
+        if self.MDEBUG:
+            print("Y list ", Y_list)
+            print("xx list ", self.x_list)
+            print("len of xx list ", len(self.x_list))
+
+        # Digest of self.Y in self.h
+        Y_bytes = self.listToBytes(Y_list)
+
+        self.hash_h.update(self.m_bytes)
+        self.hash_h.update(Y_bytes)
+        self.h_bytes = self.hash_h.digest()
+        self.h_list = self.bytesToList(self.h_bytes)
+
+        if self.MDEBUG:
+            print("self.h bytes ", self.h_bytes)
+            print("self.h list ", self.h_list)
+            print("len self.h bytes ", len(self.h_bytes))
+
+        mySign = sign(self.v_list, self.h_list, self.x_list, self.s_1)
+
+        if self.MDEBUG:
+            print("signed transaction self.v_list ", ToHexString(self.v_list).getString(), " ", self.v_list)
+            print("signed transaction self.h_list ", ToHexString(self.h_list).getString(), " ", self.h_list)
+            print("signed transaction self.x_list ", ToHexString(self.x_list).getString(), " ", self.x_list)
+            print("signed transaction self.s_1 ls ", ToHexString(self.s_1).getString(), " ", self.s_1)
+            print()
 
     def getSignature(self):
         return codecs.encode(self.signature,'hex_codec')
