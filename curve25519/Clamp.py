66e03ce07b272630de00135e911843bebf6ab10f
Alex <alessio.battistutta@gmail.com>
2017-12-12 18:30:03 +0000

    Version Alpha 0.0.03

--- curve25519/Clamp.py
+++ curve25519/Clamp.py
@@ -1,4 +1,4 @@
-
+from curve25519.Packl_ctypes import Packl_ctypes as packl_ctypes
 
 class Clamp(object):
 
@@ -7,7 +7,12 @@ class Clamp(object):
 
         :param k: list[]
         """
+        #print(k[0], k[31])
 
         k[31] &= 0x7F
         k[31] |= 0x40
         k[0] &= 0xF8
+
+        k[0] = packl_ctypes(k[0]).value
+        k[31] = packl_ctypes(k[31]).value
+        #print(k[0], k[31])
