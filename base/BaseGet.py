a0837f75a7bac40f3f4a847cb45762f96a936fd2
Alex <alessio.battistutta@gmail.com>
2017-12-14 15:57:20 +0000

    Version Alpha 0.0.04

--- base/BaseGet.py
+++ base/BaseGet.py
@@ -1,26 +1,35 @@
+# -*- coding: utf-8 -*-
 import requests
 import json
+from base import Phasing
+from base import Message
+from base import Rb
+from base import Ri
 
 class BaseGet(object):
 
-    def __init__(self, rt, data, phasing = None, message=None):
+    def __init__(self, rt, data, phasing = None, message=None, rb = None, ri = None):
 
-        # self.account = "NXT-XWQY-C2MJ-JPL8-F4BW2"
+        # self.accounts = "NXT-XWQY-C2MJ-JPL8-F4BW2"
         self.url = "http://localhost:6876/nxt"
         self.headers = {"Accept": "application/json"}
         self.dataDict = []
         self.response = None
         self.requestType = rt
-        #self.data = {"requestType": self.requestType, "account": self.account}
+        #self.data = {"requestType": self.requestType, "accounts": self.accounts}
 
         self.data = data
         self.phasing = phasing
         self.message = message
+        self.rb = rb
+        self.ri = ri
 
         # print(self.data)
         self._mergeRequestType()
         self._mergePhasingParams()
         self._mergeMessageParams()
+        self._mergeRbParams()
+        self._mergeRiParams()
 
         self.errorCode = None
         self.errorDescription = None
@@ -28,18 +37,30 @@ class BaseGet(object):
     def param(self):
         pass
 
+    def _checkAccountFormat(self, value):
+        if value[:4] == "NXT-" and value[8:9] == "-" and  value[13:14] == "-" and value[18:19] == "-":
+            return True
+
     def _mergeRequestType(self):
         if self.requestType:
             if "requestType" not in self.data:
                 self.data["requestType"] = self.requestType
 
     def _mergePhasingParams(self):
-        if self.phasing:
+        if self.phasing and isinstance(self.phasing, Phasing):
             self.data = {**self.data, **self.phasing}
 
     def _mergeMessageParams(self):
-        if self.message:
+        if self.message and isinstance(self.message, Message):
             self.data = {**self.data, **self.message}
+
+    def _mergeRbParams(self):
+        if self.rb and isinstance(self.rb, Rb):
+            self.data = {**self.data, **self.rb}
+
+    def _mergeRiParams(self):
+        if self.ri and isinstance(self.phasing, Ri):
+            self.data = {**self.data, **self.ri}
 
     def run(self):
         self.response = requests.get(self.url, params=self.data, headers=self.headers)
@@ -49,6 +70,10 @@ class BaseGet(object):
             self.errorDescription = self.dataDict["errorDescription"]
 
     def getData(self, key=None):
+        """
+        :param key: dictionary key, if None return the whole dictionary
+        :return: dictionary of data
+        """
         if key in self.dataDict:
             return self.dataDict[key]
         else:
--- base/BasePost.py
+++ base/BasePost.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 import requests
 import json
+from base import Phasing
+from base import Message
+from base import Rec
 
 class BasePost(object):
 
-    def __init__(self, rt, data, phasing = None, message=None):
+    def __init__(self, rt, data, phasing = None, message=None, rec=None):
 
         self.account = "NXT-XWQY-C2MJ-JPL8-F4BW2"
         self.url = "http://localhost:6876/nxt"
@@ -11,18 +15,24 @@ class BasePost(object):
         self.dataDict = []
         self.response = None
         self.requestType = rt
-        # self.data = {"requestType": self.requestType, "account": self.account}
+        # self.data = {"requestType": self.requestType, "accounts": self.accounts}
         self.data = data
         self.phasing = phasing
         self.message = message
+        self.rec = rec
 
         # print(self.data)
         self._mergeRequestType()
         self._mergePhasingParams()
         self._mergeMessageParams()
+        self._mergeRecParams()
 
         self.errorCode = None
         self.errorDescription = None
+
+    def _checkAccountFormat(self, value):
+        if value[:4] == "NXT-" and value[8:9] == "-" and  value[13:14] == "-" and value[18:19] == "-":
+            return True
 
     def _mergeRequestType(self):
         if self.requestType:
@@ -30,12 +40,20 @@ class BasePost(object):
                 self.data["requestType"] = self.requestType
 
     def _mergePhasingParams(self):
-        if self.phasing:
+        if self.phasing and isinstance(self.phasing, Phasing):
             self.data = {**self.data, **self.phasing}
 
     def _mergeMessageParams(self):
-        if self.message:
+        if self.message and isinstance(self.message, Message):
             self.data = {**self.data, **self.message}
+
+    def _mergeRecParams(self):
+        if self.rec and isinstance(self.rec, Rec):
+            self.data = {**self.data, **self.rec}
+
+    def _checkAccountFormat(self, value):
+        if value[:4] == "NXT-" and value[8:9] == "-" and  value[13:14] == "-" and value[18:19] == "-":
+            return True
 
     def run(self):
         self.response = requests.post(self.url, data=self.data, headers=self.headers)
