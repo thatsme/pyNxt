4fa97ef4621f8cb4a4b6c4809e61a4aa4b9ed9aa
Alex <alessio.battistutta@gmail.com>
2017-12-29 10:32:17 +0000

    Version Alpha 0.0.04

--- base/Credentials.py
+++ base/Credentials.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
 
 class Credentials(object):
 
     def __init__(self, host, account, passphrase):
+        """
 
+        :param host: Host of blockchain
+        :param account: account in RS format
+        :param passphrase: your passphrase
+        """
         self.host = host
         self.account = account
         self.passphrase = passphrase
